e8f433de2537bdf025f1334fa3b1fe8647786cc2
Pedro Fernandes <pedrofernandes.olv@gmail.com>
2026-05-15 23:48:29 -0300

    :sparkles: feat: Scan all link pages

---
+++ main.py
@@ -0,0 +1,42 @@
+import socket
+from bs4 import BeautifulSoup
+
+HOST = "localhost"
+PORT = 8080
+PATHS = [""]
+VISITED_PATHS = set()
+
+print("🕷️ Iniciando o Crawler...")
+
+while PATHS:
+    
+    path = PATHS.pop(0)
+
+    print(f"\n🔗 Visitando: http://{HOST}:{PORT}/{path}")
+    request = f"GET /{path} HTTP/1.1\r\nConnection: close\r\nHost: localhost\r\n\r\n"
+    
+    VISITED_PATHS.add(path)
+
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        s.connect((HOST, PORT))
+        s.sendall(request.encode())
+
+        response = b""
+
+        while True:
+            data = s.recv(1024)
+            if not data:
+                break
+            response += data
+
+    html_content = response.decode('utf-8', errors='ignore')
+    soup = BeautifulSoup(html_content, 'html.parser')
+
+    for link in soup.find_all('a'):
+        href = link.get('href')
+        
+        if href.startswith("/"):
+            href = href[1:]
+
+        if href not in VISITED_PATHS and href not in PATHS:   
+            PATHS.append(href)
