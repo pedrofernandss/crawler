f98fa4d0709c75e33342e9476cbc4a9464daf8b7
Pedro Fernandes <pedrofernandes.olv@gmail.com>
2026-05-16 20:17:04 -0300

    ✨ feat: Add status code treatment

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ HOST = "localhost"
 PORT = 8080
 PATHS = [""]
 VISITED_PATHS = set()
+GRAPH_REPORT = set()
 
 print("🕷️ Iniciando o Crawler...")
 
@@ -36,13 +37,31 @@ while PATHS:
     header_splited = header.split()
     status_code = header_splited[1]
 
-    soup = BeautifulSoup(html_content, 'html.parser')
+    if status_code == "301":
+        for content in html_content_list:
+            if content.startswith("Location:"):
+                line = content.split(" ")
+                redirect_url = line[1]
+                clean_path = redirect_url.removeprefix("http://localhost/")
 
-    for link in soup.find_all('a'):
-        href = link.get('href')
-        
-        if href.startswith("/"):
-            href = href[1:]
+                GRAPH_REPORT.add((path, clean_path))
+                
+                if clean_path not in VISITED_PATHS and clean_path not in PATHS:   
+                    PATHS.append(clean_path)
+    elif status_code == "404":
+        continue
+    elif status_code == "501":
+        continue
+    else:      
+        soup = BeautifulSoup(html_content, 'html.parser')
 
-        if href not in VISITED_PATHS and href not in PATHS:   
-            PATHS.append(href)
+        for link in soup.find_all('a'):
+            href = link.get('href')
+            
+            if href.startswith("/"):
+                href = href[1:]
+
+            GRAPH_REPORT.add((path, href))
+            if href not in VISITED_PATHS and href not in PATHS:   
+                PATHS.append(href)
+
